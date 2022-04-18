ec47af89fbf53eda99600cb543336a4f276e6112
Alexius Wadell <awadell@gmail.com>
2022-04-18 18:37:39 -0400

    Expand test/benchmark suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using SafeTestsets
+using PkgJogger
 
 @testset "Parareal" begin
     @safetestset "Parareal" begin include("test_parareal.jl") end
+    @safetestset "Benchmarks" begin include("test_benchmarks.jl") end
 end
--- test/test_parareal.jl
+++ test/test_parareal.jl
@@ -1,8 +1,9 @@
 using Test
 using Parareal
 using OrdinaryDiffEq
-using DiffEqProblemLibrary.ODEProblemLibrary
-ODEProblemLibrary.importodeproblems()
+using DiffEqDevTools
+
+include("problems.jl")
 
 @testset "smoke" begin
     alg = PararealAlgo(4, RK4(), RK4())
@@ -10,25 +11,26 @@ ODEProblemLibrary.importodeproblems()
 
     # Simulate
     saveat = 0.0:0.1:1.0
-    sol = solve(prob_ode_linear, alg; save_intervals=false, reltol=1e-3, abstol=1e-6, saveat)
+    sol = solve(ode_linear_problem(), alg; reltol=1e-3, abstol=1e-6, saveat)
     @test sol isa ODESolution
     @test sol.t[1] == 0.0
     @test sol.t[end] == 1.0
     @test sol.t == collect(saveat)
-    @test sol.errors[:l2] <= 1e-6
 
     # Check that reference solution is resonable
-    sol_ref = solve(prob_ode_linear, RK4(); saveat)
-    @test 10 * sol_ref.errors[:l2] >= sol.errors[:l2]
-    @test sol_ref.t == collect(saveat)
-
-    # Check that the reference solution is similar
-    e = @.((sol.u - sol_ref.u)^2)
-    l2 = sum(e) / length(e)
-    @test l2 <= 1e-6
+    sol_ref = solve(ode_linear_problem(), RK4(); saveat)
+    sol = DiffEqDevTools.appxtrue(sol, sol_ref)
+    @test sol.errors[:final] <= 1e-4
 end
 
 
-
+@testset "heat" begin
+    sol = solve(heat(10), PararealAlgo(32, Euler(), Euler()); dt=1e-4, dt_coarse=1e-2)
+    sol_ref = solve(heat(10), Euler(); dt=1e-4)
+    sol = DiffEqDevTools.appxtrue(sol, sol_ref)
+    @test sol.errors[:l2] <= 1e-4
+    @test sol.errors[:l∞] <= 1e-4
+    @test sol.errors[:final] <= 1e-4
+end
 
 
